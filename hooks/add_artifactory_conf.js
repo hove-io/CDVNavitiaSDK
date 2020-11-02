362fb4a789b32b95949dee88a89a8c6de2354853
Rachik ABIDI <rachik.abidi@kisio.org>
2020-11-02 14:36:44 +0100

    🐛 Fix credentials script

--- hooks/add_artifactory_conf.js
+++ hooks/add_artifactory_conf.js
@@ -7,35 +7,43 @@ module.exports = function(ctx) {
 	} catch (err) { }
 
 	var PLUGIN_ID = ctx.opts.plugin.id;
-
 	var _getPreferenceValue = function(key) {
+		// Check in environment variables
+		if (process.env[key]) {
+			return process.env[key]
+		}
+
+		// Check in Package.json (project) file
 		if (package && package.cordova && package.cordova.plugins && package.cordova.plugins[PLUGIN_ID] && package.cordova.plugins[PLUGIN_ID][key]) {
 			return package.cordova.plugins[PLUGIN_ID][key];
 		}
 
+		// Check in Config.xml (project) file
 		var config = fs.readFileSync('config.xml').toString();
 		var confValue = config.match(new RegExp(`"${PLUGIN_ID}"(.(?!<\/plugin>))*?<variable name="${key}" value="(.*?)".*?<\/plugin>`, 'is'));
 		if (confValue && confValue[2]) {
 			return confValue[2];
-		} else {
-			var defaultPreferences = ctx.opts.plugin.pluginInfo.getPreferences();
-			return defaultPreferences[key] || null;
 		}
+
+		// Check in plugin default preferences
+		var defaultPreferences = ctx.opts.plugin.pluginInfo.getPreferences();
+		return defaultPreferences[key] || null;
 	}
 
-	if (ctx.opts.platforms.includes('android')) {
+	if (ctx.opts.cordova.platforms.includes('android')) {
 		// Android platform: add the authentification informations into the gradle.properties file in the project
+		console.log('➕ Adding authentication credentials to Android platform')
 		var gradlePropertiesPath = './platforms/android/gradle.properties';
-		var gradleProperties = fs.readFileSync(gradlePropertiesPath);
-		gradleProperties = gradleProperties.toString();
+		var gradleProperties = fs.readFileSync(gradlePropertiesPath);		
 		if (gradleProperties) {
 			gradleProperties = gradleProperties.toString();
 			if (!gradleProperties.match('kisio_artifactory_url')) {
-				gradleProperties += `\nkisio_artifactory_url=${_getPreferenceValue('ARTIFACTORY_URL')}`;
-				gradleProperties += `\nkisio_artifactory_username=${_getPreferenceValue('ARTIFACTORY_USERNAME')}`;
-				gradleProperties += `\nkisio_artifactory_password=${_getPreferenceValue('ARTIFACTORY_PASSWORD')}`;
-				gradleProperties += `\nkisio_artifactory_android_repo_release=${_getPreferenceValue('ARTIFACTORY_ANDROID_REPO_RELEASE')}`;
-				gradleProperties += `\nkisio_artifactory_android_repo_snapshot=${_getPreferenceValue('ARTIFACTORY_ANDROID_REPO_SNAPSHOT')}`;
+				gradleProperties += `\nkisio_artifactory_url=${_getPreferenceValue('KISIO_ARTIFACTORY_URL')}`;
+				gradleProperties += `\nkisio_artifactory_username=${_getPreferenceValue('KISIO_ARTIFACTORY_USERNAME')}`;
+				gradleProperties += `\nkisio_artifactory_password=${_getPreferenceValue('KISIO_ARTIFACTORY_PASSWORD')}`;
+				gradleProperties += `\nkisio_artifactory_android_repo_release=${_getPreferenceValue('KISIO_ARTIFACTORY_ANDROID_REPO_RELEASE')}`;
+				gradleProperties += `\nkisio_artifactory_android_repo_snapshot=${_getPreferenceValue('KISIO_ARTIFACTORY_ANDROID_REPO_SNAPSHOT')}`;
+
 				fs.writeFileSync(gradlePropertiesPath, gradleProperties, 'utf8');
 			}
 		} else {
@@ -43,12 +51,13 @@ module.exports = function(ctx) {
 		}
 	}
 
-	if (ctx.opts.platforms.includes('ios')) {
+	if (ctx.opts.cordova.platforms.includes('ios')) {
 		// IOS platform: add the authentification informations into the .netrc file (on the home directory)
+		console.log('➕ Adding authentication credentials to iOS platform')
 		var netrcPath = os.homedir() + '/.netrc';
-		var machine = _getPreferenceValue('ARTIFACTORY_URL').match(/^https?:\/\/([^:\/?#]*)/)[1];
-		var netrcLine = `machine ${machine} login ${_getPreferenceValue('ARTIFACTORY_USERNAME')} password ${_getPreferenceValue('ARTIFACTORY_PASSWORD')}\n`;
+		var machine = _getPreferenceValue('KISIO_ARTIFACTORY_URL').match(/^https?:\/\/([^:\/?#]*)/)[1];
 		if (machine) {
+			var netrcLine = `machine ${machine} login ${_getPreferenceValue('KISIO_ARTIFACTORY_USERNAME')} password ${_getPreferenceValue('KISIO_ARTIFACTORY_PASSWORD')}\n`;
 			var netrcContent = '';
 			if (fs.existsSync(netrcPath)) {
 				var netrcContent = fs.readFileSync(netrcPath).toString() || '';
@@ -60,6 +69,7 @@ module.exports = function(ctx) {
 			} else {
 				netrcContent += netrcLine;
 			}
+
 			fs.writeFileSync(netrcPath, netrcContent, 'utf8');
 		}
 	}
